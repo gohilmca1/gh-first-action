b43c3fa8d4e2dd78d71af60e06aec0859c05d471
gohilmca1 <gohil_mca@yahoo.com>
2026-04-24 15:25:47 +0530

    Update C.java

--- C.java
+++ C.java
@@ -3,7 +3,10 @@ public class C {
 //zinga
 	  long start = System.currentTimeMillis();
         System.out.println("Jay Mataji... from Java file");
-		  
+
+	 	String myVar = System.getenv("MY_VAR");
+	  System.out.println("first:"+myVar);
+
       long end = System.currentTimeMillis();
       System.out.println("Time taken "+ (end - start));
     }
